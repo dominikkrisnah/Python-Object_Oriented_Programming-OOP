4be446f497992428eb39ca5bd62911e8994c4652
Dominikus Krisna Herlambang <krisnaherlambang48@gmail.com>
2026-04-08 10:47:15 +0700

    Add files via upload

---
+++ Part_03-Class_&_Instance_Attributes.py
@@ -0,0 +1,21 @@
+class Hero: #template
+	#class variabel
+	jumlah = 0
+
+	def __init__(self,inputName,inputHealth,inputPower,inputArmor):
+		# instance variabel
+		self.name = inputName
+		self.health = inputHealth
+		self.power = inputPower
+		self.armor = inputArmor
+		Hero.jumlah += 1
+		print("membuat Hero dengan nama " + inputName)
+
+
+hero1 = Hero("Albeta",100, 10, 4)
+print(Hero.jumlah)
+hero2 = Hero("Krisna",100, 15, 1)
+print(Hero.jumlah)
+hero3 = Hero("Dominik",1000, 100, 0)
+print(Hero.jumlah)
+
---
+++ Part_15b-Inheritance_Practice.py
@@ -0,0 +1,14 @@
+from Part_15 import HeroIntelligent,HeroStrength
+
+
+kirana = HeroIntelligent('kirana')
+felia = HeroStrength('felia')
+
+kirana.show_info()
+felia.show_info()
+
+kirana.gainExp = 200
+felia.gainExp = 250
+
+kirana.show_info()
+felia.show_info()
